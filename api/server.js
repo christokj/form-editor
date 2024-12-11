71fc6e20fc1727869e10de9a42b29f5c677a693e
Christo K J <christokj2@gmail.com>
2024-12-11 13:05:18 +0530

    Basic folder structure completed

---
+++ api/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Routes
+
+app.use('/api/users', userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
