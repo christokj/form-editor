0037e2d788c48a87cd64e8bd7a8038d62d0492ea
Christo K J <christokj2@gmail.com>
2024-12-11 12:44:45 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { RouterProvider } from "react-router-dom";
+import './App.css'
+import { Toaster } from "react-hot-toast";
+import { router } from './routes/Routes';
+
+function App() {
+
+
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Toaster />
+    </>
+  );
+}
+
+export default App;
